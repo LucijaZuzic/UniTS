d30f5ef4944542fd1d178282c0cbb4a2da0f4ed0
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-03-31 17:23:38 +0200

    new

--- read_res.py
+++ read_res.py
@@ -2,20 +2,14 @@ import os
 import pickle
 import numpy as np
 from utils.metrics import metric
-for num in range(2, 7): 
-    for file_val in os.listdir("results_" + str(num) + "_val"):
+for num in range(2, 3): 
+    for file_val in os.listdir("results_all_" + str(num) + "_train"):
         if "preds"in file_val:
             continue
-        with open("results_" + str(num) + "_val/" + file_val, 'rb') as file_object:
+        with open("results_all_" + str(num) + "_train/" + file_val, 'rb') as file_object:
             trues = pickle.load(file_object) 
             file_object.close() 
-            print("results_" + str(num) + "_val/" + file_val, np.shape(trues))
-            print(trues[0])
-        with open("results_" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds = pickle.load(file_object) 
-            file_object.close() 
-            print("results_" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds))
-            print(preds[0])
-        mae, mse, rmse, mape, mspe = metric(preds, trues)
-        print(mae, mse, rmse, mape, mspe)
+            print("results_all_" + str(num) + "_train/" + file_val, np.shape(trues))
+            print(trues[0]) 
+        break
         
