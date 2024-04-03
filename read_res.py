1e4ffc86a097e949b11bfe8ac0a36b7adcacdf64
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-04-03 12:56:34 +0200

    Update read_res.py

--- read_res.py
+++ read_res.py
@@ -4,6 +4,39 @@ import numpy as np
 import pandas as pd
 from utils.metrics import metric
 
+def transform_pd_file(pd_file):
+    pd_file = pd_file.drop(labels = "date", axis = 1)
+    pd_file = pd_file.to_numpy()
+    return pd_file 
+
+def transform_np_file(np_file):
+    np_file = np.array(np_file).squeeze()
+    return np_file 
+
+def find_match(transformed_pd_file, transformed_np_file):
+
+    matches_np_pd = dict()
+    matches_pd_np = dict()
+
+    for ix_np_entry in range(np.shape(transformed_np_file)[0]):
+        matches_np_pd[ix_np_entry] = -1
+
+    for ix_pd_entry in range(np.shape(transformed_pd_file)[0]):
+        matches_pd_np[ix_pd_entry] = -1
+
+    for ix_np_entry in range(np.shape(transformed_np_file)[0]):
+
+        np_entry = transformed_np_file[ix_np_entry]
+ 
+        new_pd_file = transformed_pd_file - np_entry
+        new_pd_file = [sum([abs(new_pd_file[ix1][ix2]) for ix2 in range(len(new_pd_file[ix1]))]) for ix1 in range(len(new_pd_file))]
+        min_pd_entry = min(new_pd_file)
+        ix_pd_entry = new_pd_file.index(min_pd_entry)
+        matches_np_pd[ix_np_entry] = ix_pd_entry
+        matches_pd_np[ix_pd_entry] = ix_np_entry
+
+    return matches_np_pd, matches_pd_np
+
 for num in range(2, 7): 
 
     for file_val in os.listdir("results/" + str(num) + "_val"):
@@ -17,192 +50,112 @@ for num in range(2, 7):
         varname = file_val.replace("trues_transformed_", "")
 
         pd_file_train = pd.read_csv("dataset_new/" + str(num) + "/" + varname + "/newdata_TRAIN.csv")
+        pd_file_train_transformed = transform_pd_file(pd_file_train)
         pd_file_val = pd.read_csv("dataset_new/" + str(num) + "/" + varname + "/newdata_VAL.csv")
+        pd_file_val_transformed = transform_pd_file(pd_file_val)
         pd_file_test = pd.read_csv("dataset_new/" + str(num) + "/" + varname + "/newdata_TEST.csv")
-
-        print(varname, num)
-        with open("results/" + str(num) + "_val/" + file_val, 'rb') as file_object:
-            trues_S_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_val/" + file_val, np.shape(trues_S_all_val))
-            #print(trues_S_all_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(trues_S_all_val))
-        with open("results/" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds_S_all_val))
-            #print(preds_S_all_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(preds_S_all_val))
-        continue
-
-        for i in range(30):
-            lv = []
-            for j in range(num):
-                lv.append(pd_file_val[str(j)][i])
-            lv.append(pd_file_val["OT"][i])
-            print(lv, trues_S_all_val[i], preds_S_all_val[i])
-            break
-
-        for i in range(len(pd_file_val["OT"])):
-            lv = []
-            for j in range(num):
-                lv.append(pd_file_val[str(j)][i])
-            lv.append(pd_file_val["OT"][i])
-            if (lv[0] == 203 and lv[1] == 203):
-                print(lv)
-
+        pd_file_test_transformed = transform_pd_file(pd_file_test)
+ 
         with open("results/all_" + str(num) + "_train/" + file_val, 'rb') as file_object:
             trues_all_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_train/" + file_val, np.shape(trues_all_train))
-            #print(trues_all_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(trues_all_train))
+            file_object.close()
+        trues_all_train = transform_np_file(trues_all_train)
+        matches_np_pd_trues_all_train, matches_pd_np_trues_all_train = find_match(pd_file_train_transformed, trues_all_train)
+         
+        with open("results/all_" + str(num) + "_train/" + file_val + "_matches_np_pd_trues_all_train", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_all_train, file_object) 
+            file_object.close()
+        with open("results/all_" + str(num) + "_train/" + file_val + "_matches_pd_np_trues_all_train", 'wb') as file_object:
+            pickle.dump(matches_pd_np_trues_all_train, file_object) 
+            file_object.close()
+
         with open("results/all_" + str(num) + "_val/" + file_val, 'rb') as file_object:
             trues_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_val/" + file_val, np.shape(trues_all_val))
-            #print(trues_all_val[0])
-        print(len(pd_file_val["OT"]), np.shape(trues_all_val))
+            file_object.close()
+        trues_all_val = transform_np_file(trues_all_val)
+        matches_np_pd_trues_all_val, matches_pd_np_trues_all_val = find_match(pd_file_val_transformed, trues_all_val)
+        
+        with open("results/all_" + str(num) + "_val/" + file_val + "_matches_np_pd_trues_all_val", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_all_val, file_object) 
+            file_object.close()
+        with open("results/all_" + str(num) + "_val/" + file_val + "_matches_np_pd_trues_all_val", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_all_val, file_object) 
+            file_object.close()
+
         with open("results/all_" + str(num) + "_test/" + file_val, 'rb') as file_object:
             trues_all_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_test/" + file_val, np.shape(trues_all_test))
-            #print(trues_all_test[0]) 
-        print(len(pd_file_test["OT"]), np.shape(trues_all_test))
-        
+            file_object.close()
+        trues_all_test = transform_np_file(trues_all_test)
+        matches_np_pd_trues_all_test, matches_pd_np_trues_all_test = find_match(pd_file_test_transformed, trues_all_test)
+
+        with open("results/all_" + str(num) + "_test/" + file_val + "_matches_np_pd_trues_all_test", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_all_test, file_object) 
+            file_object.close()
+        with open("results/all_" + str(num) + "_test/" + file_val + "_matches_np_pd_trues_all_test", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_all_test, file_object) 
+            file_object.close()
+
         with open("results/" + str(num) + "_train/" + file_val, 'rb') as file_object:
             trues_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_train/" + file_val, np.shape(trues_train))
-            #print(trues_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(trues_train))
+            file_object.close()
+        trues_train = transform_np_file(trues_train)
+        matches_np_pd_trues_train, matches_pd_np_trues_train = find_match(pd_file_train_transformed, trues_train)
+         
+        with open("results/" + str(num) + "_train/" + file_val + "_matches_np_pd_trues_train", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_train, file_object) 
+            file_object.close()
+        with open("results/" + str(num) + "_train/" + file_val + "_matches_pd_np_trues_train", 'wb') as file_object:
+            pickle.dump(matches_pd_np_trues_train, file_object) 
+            file_object.close()
+
         with open("results/" + str(num) + "_val/" + file_val, 'rb') as file_object:
             trues_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_val/" + file_val, np.shape(trues_val))
-            #print(trues_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(trues_val))
+            file_object.close()
+        trues_val = transform_np_file(trues_val)
+        matches_np_pd_trues_val, matches_pd_np_trues_val = find_match(pd_file_val_transformed, trues_val)
+        
+        with open("results/" + str(num) + "_val/" + file_val + "_matches_np_pd_trues_val", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_val, file_object) 
+            file_object.close()
+        with open("results/" + str(num) + "_val/" + file_val + "_matches_np_pd_trues_val", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_val, file_object) 
+            file_object.close()
+
         with open("results/" + str(num) + "_test/" + file_val, 'rb') as file_object:
             trues_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_test/" + file_val, np.shape(trues_test))
-            #print(trues_test[0])
-        print(len(pd_file_test["OT"]), np.shape(trues_test))
+            file_object.close()
+        trues_test = transform_np_file(trues_test)
+        matches_np_pd_trues_test, matches_pd_np_trues_test = find_match(pd_file_test_transformed, trues_test)
+
+        with open("results/" + str(num) + "_test/" + file_val + "_matches_np_pd_trues_test", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_test, file_object) 
+            file_object.close()
+        with open("results/" + str(num) + "_test/" + file_val + "_matches_np_pd_trues_test", 'wb') as file_object:
+            pickle.dump(matches_np_pd_trues_test, file_object) 
+            file_object.close()
 
         with open("results/all_" + str(num) + "_train/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_all_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_train/" + file_val.replace("trues", "preds"), np.shape(preds_all_train))
-            #print(preds_all_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(preds_all_train))
+            file_object.close()
+        preds_all_train = transform_np_file(preds_all_train)
         with open("results/all_" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds_all_val))
-            #print(preds_all_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(preds_all_val))
+            file_object.close()
+        preds_all_val = transform_np_file(preds_all_val)
         with open("results/all_" + str(num) + "_test/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_all_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/all_" + str(num) + "_test/" + file_val.replace("trues", "preds"), np.shape(preds_all_test))
-            #print(preds_all_test[0]) 
-        print(len(pd_file_test["OT"]), np.shape(preds_all_test))
+            file_object.close()
+        preds_all_test = transform_np_file(preds_all_test)
         
         with open("results/" + str(num) + "_train/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_train/" + file_val.replace("trues", "preds"), np.shape(preds_train))
-            #print(preds_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(preds_train))
+            file_object.close()
+        preds_train = transform_np_file(preds_train)
         with open("results/" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds_val))
-            #print(preds_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(preds_val))
+            file_object.close()
+        preds_val = transform_np_file(preds_val)
         with open("results/" + str(num) + "_test/" + file_val.replace("trues", "preds"), 'rb') as file_object:
             preds_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/" + str(num) + "_test/" + file_val.replace("trues", "preds"), np.shape(preds_test))
-            #print(preds_test[0])
-        print(len(pd_file_test["OT"]), np.shape(preds_test))
- 
-        with open("results/S_all_" + str(num) + "_train/" + file_val, 'rb') as file_object:
-            trues_S_all_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_train/" + file_val, np.shape(trues_S_all_train))
-            #print(trues_S_all_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(trues_S_all_train))
-        with open("results/S_all_" + str(num) + "_val/" + file_val, 'rb') as file_object:
-            trues_S_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_val/" + file_val, np.shape(trues_S_all_val))
-            #print(trues_S_all_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(trues_S_all_val))
-        with open("results/S_all_" + str(num) + "_test/" + file_val, 'rb') as file_object:
-            trues_S_all_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_test/" + file_val, np.shape(trues_S_all_test))
-            #print(trues_S_all_test[0]) 
-        print(len(pd_file_test["OT"]), np.shape(trues_S_all_test))
-        
-        with open("results/S_" + str(num) + "_train/" + file_val, 'rb') as file_object:
-            trues_S_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_train/" + file_val, np.shape(trues_S_train))
-            #print(trues_S_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(trues_S_train))
-        with open("results/S_" + str(num) + "_val/" + file_val, 'rb') as file_object:
-            trues_S_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_val/" + file_val, np.shape(trues_S_val))
-            #print(trues_S_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(trues_S_val))
-        with open("results/S_" + str(num) + "_test/" + file_val, 'rb') as file_object:
-            trues_S_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_test/" + file_val, np.shape(trues_S_test))
-            #print(trues_S_test[0])
-        print(len(pd_file_test["OT"]), np.shape(trues_S_test))
-
-        with open("results/S_all_" + str(num) + "_train/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_all_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_train/" + file_val.replace("trues", "preds"), np.shape(preds_S_all_train))
-            #print(preds_S_all_train[0]) 
-        print(len(pd_file_train["OT"]), np.shape(preds_S_all_train))
-        with open("results/S_all_" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_all_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds_S_all_val))
-            #print(preds_S_all_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(preds_S_all_val))
-        with open("results/S_all_" + str(num) + "_test/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_all_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_all_" + str(num) + "_test/" + file_val.replace("trues", "preds"), np.shape(preds_S_all_test))
-            #print(preds_S_all_test[0]) 
-        print(len(pd_file_test["OT"]), np.shape(preds_S_all_test))
-        
-        with open("results/S_" + str(num) + "_train/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_train = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_train/" + file_val.replace("trues", "preds"), np.shape(preds_S_train))
-            #print(preds_S_train[0]) 
-        print(len(pd_file_test["OT"]), np.shape(preds_S_train))
-        with open("results/S_" + str(num) + "_val/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_val = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_val/" + file_val.replace("trues", "preds"), np.shape(preds_S_val))
-            #print(preds_S_val[0]) 
-        print(len(pd_file_val["OT"]), np.shape(preds_S_val))
-        with open("results/S_" + str(num) + "_test/" + file_val.replace("trues", "preds"), 'rb') as file_object:
-            preds_S_test = pickle.load(file_object) 
-            file_object.close() 
-            #print("results/S_" + str(num) + "_test/" + file_val.replace("trues", "preds"), np.shape(preds_S_test))
-            #print(preds_S_test[0])
-        print(len(pd_file_test["OT"]), np.shape(preds_S_test))
-
-        break
-         
-    break
+            file_object.close()
+        preds_test = transform_np_file(preds_test)
